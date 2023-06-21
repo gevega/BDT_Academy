4ebe2e32fb8608e57882cb721e0f75d03f347d79
gevega <gevegaecheverria@gmail.com>
2023-06-21 12:06:24 -0300

    Parcial 12:06

---
+++ Tareas/06-2023/21/Vega-Gonzalo/Program.cs
@@ -0,0 +1,13 @@
+﻿//INICIO PROGRAMA PRINCIPAL
+const string TEXTO = "Ingrese un nombre: ";
+string nom = "Messi";
+
+ingresar(ref nom, TEXTO);
+
+//FIN PROGRAMA PRINCIPAL
+
+static void ingresar(ref string var, string TEXTO)
+{
+    Console.Write(TEXTO);
+    var = Console.ReadLine()!;
+}
